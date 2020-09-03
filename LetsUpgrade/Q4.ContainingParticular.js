1bebbcb6d1f9fd71014fc1567bf89f7397eb9396
dheeraj9779 <57933209+dheeraj9779@users.noreply.github.com>
2020-09-04 00:03:17 +0530

    Update Q4.ContainingParticular.js

--- LetsUpgrade/Q4.ContainingParticular.js
+++ LetsUpgrade/Q4.ContainingParticular.js
@@ -1,12 +1,11 @@
-let Demo = ["reman","proof","guitar","fluffy","car","lock"];
+//Program to display only elements containing "a" in them from a Array
+
+let Demo = ["reman","proof","guitar","fluffy","car","lock","train"];
 let newArray = [];
-for(let i=1;i<=Demo.length;i++){
-   if( Demo[i].includes("a")){
+for(let i=0;i<Demo.length;i++){
+   if( Demo[i].includes('a')){
        newArray.push(Demo[i]);
    }
 }
+
 console.log(newArray);
-const val = Demo.filter((elem) => {
-    return elem.includes("a");
-})
-console.log(val);
